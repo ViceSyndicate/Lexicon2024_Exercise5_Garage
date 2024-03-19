e3575789badaf67bd21d5dd7b9b9b603844c4908
Victor S <victor.skarerverket@gmail.com>
2024-03-19 23:56:10 +0100

    Initial commit

---
+++ Lexicon2024_Exercise5_Garage/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Lexicon2024_Exercise5_Garage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            
+        }
+    }
+}
