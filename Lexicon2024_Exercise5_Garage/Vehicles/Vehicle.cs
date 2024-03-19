0d3b560ef50f82a3e77e6da69815e8b74a77a38c
Victor S <victor.skarerverket@gmail.com>
2024-03-20 00:19:10 +0100

    Added some base classes/structure.

---
+++ Lexicon2024_Exercise5_Garage/Vehicles/Vehicle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lexicon2024_Exercise5_Garage.Vehicles
+{
+    internal class Vehicle
+    {
+        public string RegistrationNumber { get; set; } // Unique
+        public string Color { get; set; }
+        public int Wheels { get; set; }
+        public int PassengerLimit { get; set; }
+    }
+}
